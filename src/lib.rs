67591326e76c8679d86a7b5e04e467b0f93e7d2b
Wouter Jehee <wouterjehee@tutanota.com>
2023-11-06 21:02:08 +0100

    make fetching and displaying entries work, only id for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 use cfg_if::cfg_if;
 
 pub mod model;
+pub mod app;
 
 cfg_if! { if #[cfg(feature = "hydrate")] {
     use leptos::*;
     use wasm_bindgen::prelude::wasm_bindgen;
     use crate::app::*;
-
-    pub mod app;
 
     #[wasm_bindgen]
     pub fn hydrate() {
