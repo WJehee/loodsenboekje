5e2abb4ea3e2064af72d677797baafdffdb1413c
Wouter Jehee <wouterjehee@tutanota.com>
2023-11-13 20:10:50 +0100

    change permissions to boolean, add account creation passwords

--- src/model/user.rs
+++ src/model/user.rs
@@ -1,33 +1,17 @@
 use serde::{Deserialize, Serialize};
 use leptos::*;
 use cfg_if::cfg_if;
-use strum::{Display, EnumString};
 
 cfg_if! { if #[cfg(feature = "ssr")] {
     use super::db;
     use sqlx::FromRow;
-
-    async fn get_permissions(user_id: i64) -> Option<Vec<SqlPermission>> {
-        let db = db().await;
-       sqlx::query_as!(SqlPermission, "SELECT permission FROM user_permissions WHERE user_id = ?", user_id)
-           .fetch_all(&db)
-           .await.ok()
-    }
 }}
-
-#[derive(Debug, Display, Deserialize, Serialize, EnumString)]
-enum Permission {
-    View,
-    Delete,
-    Add,
-    // Update,
-}
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
     pub id: i64,
     pub name: String,
-    permissions: Vec<Permission>,
+    pub is_writer: bool,
 }
 
 #[cfg_attr(feature = "ssr", derive(FromRow))]
@@ -35,31 +19,45 @@ pub struct SqlUser {
     pub id: i64,
     pub username: String,
     pub password: String,
+    pub is_writer: i64,
 }
 
-#[cfg_attr(feature = "ssr", derive(FromRow))]
-struct SqlPermission {
-    permission: String,
+impl From<SqlUser> for User {
+    fn from(sqluser: SqlUser) -> Self {
+        User {
+            id: sqluser.id,
+            name: sqluser.username,
+            // SQLite stores bool as int, 0 = false, 1 = true
+            is_writer: sqluser.is_writer == 1,
+        }
+    }
 }
 
 #[server(Register)]
-pub async fn create_user(username: String, password: String) -> Result<i64, ServerFnError> {
+pub async fn create_user(username: String, password: String, creation_password: String) -> Result<i64, ServerFnError> {
     use bcrypt::{hash, DEFAULT_COST};
+    use std::env;
+
+    let read_password = env::var("READ_PASSWORD").unwrap();
+    let write_password = env::var("WRITE_PASSWORD").unwrap();
+
+    // TODO: error message to inform user if account creation password is invalid
+    let is_writer = match creation_password {
+        p if p == read_password => Ok(false),
+        p if p == write_password => Ok(true),
+        _ => {
+            eprintln!("Invalid account creation password");
+            Err(ServerFnError::ServerError("Invalid account creation password".into()))
+        },
+    }?;
 
     let db = db().await;
     let hashed_password = hash(password, DEFAULT_COST).unwrap();
     // TODO: error message for duplicate users
-    let id: i64 = sqlx::query!("INSERT INTO users (username, password) VALUES (?, ?)", username, hashed_password) 
+    let id: i64 = sqlx::query!("INSERT INTO users (username, password, is_writer) VALUES (?, ?, ?)", username, hashed_password, is_writer)
         .execute(&db)
         .await?
         .last_insert_rowid();
-
-    // TODO: check for user creation password, defined in .env, this will dictate the user
-    // permissions upon creation (either view, or all 3)
-    let perm = Permission::View.to_string();
-    sqlx::query!("INSERT INTO user_permissions (user_id, permission) VALUES (?, ?)", id, perm)
-        .execute(&db)
-        .await?;
 
     println!("created user: '{username}', with id: '{id}'");
     leptos_axum::redirect("/login");
@@ -68,24 +66,11 @@ pub async fn create_user(username: String, password: String) -> Result<i64, Serv
 
 #[server]
 pub async fn get_user(id: i64) -> Result<User, ServerFnError> {
-    use std::str::FromStr;
-
     let db = db().await;
     let sqluser = sqlx::query_as!(SqlUser, "SELECT * FROM users WHERE id = ?", id)
         .fetch_one(&db)
         .await?;
-    // TODO: Implement this as a function for the struct
-    let result = User {
-        id: sqluser.id,
-        name: sqluser.username,
-        permissions: get_permissions(sqluser.id)
-            .await
-            .unwrap()
-            .iter()
-            .map(|p| Permission::from_str(&p.permission).unwrap())
-            .collect(), 
-    };
-    Ok(result)
+    Ok(sqluser.into())
 }
 
 #[server]
