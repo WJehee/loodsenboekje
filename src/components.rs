e58dce2d02b2b10714e89c5c430fb5b26747c142
Wouter Jehee <wouter@wouterjehee.com>
2023-11-27 13:36:45 +0100

    add logger, closes #25, refactor, provide user as context

--- src/components.rs
+++ src/components.rs
@@ -2,7 +2,7 @@ use leptos::*;
 use leptos_router::*;
 use chrono::Datelike;
 
-use crate::model::{entry::{Entry, AddEntry, get_entries, validate_who, DeleteEntry}, user::{get_all_users, UserType}};
+use crate::model::{entry::{Entry, AddEntry, get_entries, validate_who, DeleteEntry}, user::{get_all_users, UserType, User}};
 
 #[component]
 pub fn AddEntryForm(
@@ -173,8 +173,8 @@ fn EntryRow(
     entry: Entry,
     delete_entry: Action<DeleteEntry, Result<(), ServerFnError>>,
 ) -> impl IntoView {
-    let user_type = use_context::<RwSignal<UserType>>()
-        .expect("to have can_write read signal set");
+    let user = use_context::<Resource<(usize, usize, usize), Result<Option<User>, ServerFnError>>>()
+        .expect("to have user set in context");
     view! {
         <tr>
             <td scope="row">{ entry.id }</td>
@@ -188,17 +188,20 @@ fn EntryRow(
                 &entry.created.year(),
             )}</td>
             <td>
-                { move || match user_type.get() {
-                    UserType::Writer | UserType::Admin => view! {
-                        <ActionForm action=delete_entry>
-                            <input type="hidden" name="id" value={entry.id}/>
-                            <button type="submit" name="submit" class="outline secondary">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-                            </button>
-                        </ActionForm>
+                { move || user.get().map(|user| match user {
+                    Ok(Some(user)) => match user.user_type {
+                        UserType::Writer | UserType::Admin => view! {
+                            <ActionForm action=delete_entry>
+                                <input type="hidden" name="id" value={entry.id}/>
+                                <button type="submit" name="submit" class="outline secondary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+                                </button>
+                            </ActionForm>
+                        },
+                        UserType::Reader | UserType::Inactive => ().into_view(),
                     },
-                    UserType::Reader | UserType::Inactive => ().into_view(),
-                }}
+                    _ => ().into_view()
+                })}
             </td>
         </tr>
     }
