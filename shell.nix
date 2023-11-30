b66a512125f4b2e1daeabcba391848d2638892cc
Wouter Jehee <wouterjehee@tutanota.com>
2023-11-30 16:50:13 +0100

    deploy and readme updates

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "dev-environment";
-    buildInputs = [ pkg-config sqlx-cli openssl cargo-leptos binaryen ];
+    buildInputs = [ cargo pkg-config sqlx-cli openssl cargo-leptos binaryen ];
     LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
     shellHook = ''
         export DATABASE_URL="sqlite://sqlite.db"
