6cb950a4810e64cc765553b45b32bf8bd224223e
Wouter Jehee <wouter@wouterjehee.com>
2024-03-01 15:05:14 +0100

    switch to flake

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-    name = "dev-environment";
-    buildInputs = [ pkg-config sqlx-cli openssl cargo-leptos binaryen ];
-    LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
-    shellHook = ''
-        export DATABASE_URL="sqlite://sqlite.db"
-    '';
-}
