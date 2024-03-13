a974d1d4974edb0ec99c0b29fe636a4297130246
Wouter Jehee <wouterjehee@tutanota.com>
2024-03-13 22:48:36 +0100

    separate package.nix file and add module

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
         rust-overlay.url = "github:oxalica/rust-overlay";
-        crane-flake = {
+        crane = {
             url = "github:ipetkov/crane";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = { self, nixpkgs, rust-overlay, crane-flake }:
+    outputs = { self, nixpkgs, rust-overlay, crane }:
     let
         system = "x86_64-linux";
         overlays = [ (import rust-overlay) ];
@@ -21,57 +21,10 @@
             extensions = [ "rust-src" ];
             targets = [ "wasm32-unknown-unknown" ];
         };
-        craneLib = (crane-flake.mkLib pkgs).overrideToolchain rust-toolchain;
-
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        inherit (cargoToml.package) name version;
-
-        args = {
-            src = craneLib.path ./.;
-            pname = name;
-            version = version;
-            # Required build inputs
-            buildInputs = with pkgs; [
-                rust-toolchain
-                cargo-leptos
-                binaryen
-                openssl
-            ];
-        };
-
-        LOODSENBOEKJE_DATA_DIR = "/var/lib/${name}";
-        DATABASE_URL = "sqlite.db";
-
-        loodsenboekje = craneLib.buildPackage (args // {
-            pname = name;
-            nativeBuildInputs = with pkgs; [
-                makeWrapper
-                sqlx-cli
-            ];
-
-            doCheck = false;
-            cargoArtifacts = craneLib.buildDepsOnly args;
-
-            preBuild = ''
-                export DATABASE_URL='sqlite://${DATABASE_URL}'
-                sqlx database create
-                sqlx migrate run
-            '';
-            buildPhaseCargoCommand = "cargo leptos build --release -vvv";
-            installPhaseCommand = ''
-                mkdir -p $out/
-                cp target/release/${name} $out/
-                cp -r target/site $out/
-                wrapProgram $out/${name} \
-                --set LEPTOS_SITE_ROOT $out/site \
-                --set READ_PASSWORD $(openssl rand -base64 32) \
-                --set WRITE_PASSWORD $(openssl rand -base64 32) \
-                --set ADMIN_PASSWORD $(openssl rand -base64 32) \
-                --set DATA_DIR ${LOODSENBOEKJE_DATA_DIR}
-            '';
-        });
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
     in {
         devShells.${system}.default = with pkgs; mkShell {
+            # TODO: make this combine with universal required build inputs
             buildInputs = [
                 rust-toolchain
                 cargo
@@ -90,10 +43,14 @@
             LD_LIBRARY_PATH = lib.makeLibraryPath [ openssl ];
             shellHook = ''
                 export DATA_DIR='./.'
-                export DATABASE_URL='sqlite://${DATABASE_URL}'
+                export DATABASE_URL='sqlite://sqlite.db'
             '';
         };
-        packages.${system}.default = loodsenboekje;
+        packages.${system}.default = pkgs.callPackage ./nix/package.nix {
+            rust-toolchain = rust-toolchain;
+            craneLib = craneLib;
+        };
+        nixosModules.loodsenboekje = import ./nix/module.nix self;
     };
 }
 
---
+++ nix/module.nix
@@ -0,0 +1,44 @@
+flake: { config, system, lib }:
+let
+    inherit (lib) mkEnableOption mkOption mdDoc types;
+    inherit (flake.packages.${system}) loodsenboekje;
+
+    cfg = config.services.loodsenboekje;
+in {
+    options = {
+        services.loodsenboekje = {
+            enable = mkEnableOption "Loodsenboekje";
+        };
+        dataDir = mkOption {
+            type = types.str;
+            default = "/var/lib/loodsenboekje";
+            description = mdDoc "Path where data and logs will be stored";
+        };
+        package = mkOption {
+            type = types.package;
+            default = loodsenboekje;
+            description = mdDoc "The package to use";
+        };
+    };
+    config = {
+        users = {
+            users.loodsenboekje = {
+                description = "Loodsenboekje daemon";
+                isSystemUser = true;
+                group = "loodsenboekje";
+            };
+            groups.loodsenboekje = {};
+        };
+        systemd.services.loodsenboekje = {
+            serviceConfig = {
+                Type = "simple";
+                User = "loodsenboekje";
+                Group = "loodsenboekje";
+
+                Restart = "always";
+                ExecStart = "DATA_DIR=${cfg.dataDir} ${lib.getBin cfg.package}/bin/loodsenboekje";
+                StateDirectory = "loodsenboekje";
+            };
+        };
+    };
+}
