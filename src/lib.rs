78062f37b0c3902af1a58739e49cd7dc20e24b17
Wouter Jehee <wouterjehee@tutanota.com>
2024-03-16 23:17:10 +0100

    update version to fix issue with wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ cfg_if! { if #[cfg(feature = "hydrate")] {
 
     #[wasm_bindgen]
     pub fn hydrate() {
+        _ = console_log::init_with_level(log::Level::Debug);
+        console_error_panic_hook::set_once();
         leptos::mount_to_body(App);
     }
 }}
